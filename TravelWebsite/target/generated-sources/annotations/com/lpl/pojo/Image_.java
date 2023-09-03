70940a791988cde6966db8fa7b80fa171419152f
lephilong190702 <lephilong1907@gmail.com>
2023-09-03 11:23:20 +0700

    update

--- TravelWebsite/target/generated-sources/annotations/com/lpl/pojo/Image_.java
+++ TravelWebsite/target/generated-sources/annotations/com/lpl/pojo/Image_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-03T11:16:24")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-03T11:21:11")
 @StaticMetamodel(Image.class)
 public class Image_ { 
 
