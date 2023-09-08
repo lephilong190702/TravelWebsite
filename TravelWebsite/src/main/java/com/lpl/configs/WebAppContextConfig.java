1fb1ad4a68faa85dd859f53309adf84b330896a5
Lê Phi Long <135942675+lephilong190702@users.noreply.github.com>
2023-09-09 05:07:45 +0700

    Update WebAppContextConfig.java

--- TravelWebsite/src/main/java/com/lpl/configs/WebAppContextConfig.java
+++ TravelWebsite/src/main/java/com/lpl/configs/WebAppContextConfig.java
@@ -127,8 +127,8 @@ public class WebAppContextConfig implements WebMvcConfigurer {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("2051012056long@ou.edu.vn");
-        mailSender.setPassword("2858831551907");
+        mailSender.setUsername("");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
